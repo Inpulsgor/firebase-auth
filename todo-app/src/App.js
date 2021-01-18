b70e1a73b7373552eca7b8993cf4a078d6db8913
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2021-01-18 10:21:15 +0200

    todo-app

---
+++ todo-app/src/App.js
@@ -0,0 +1,15 @@
+import "./scss/App.scss";
+
+import Sidebar from "./components/Sidebar/Sidebar";
+import Content from "./components/Content/Content";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Sidebar />
+      <Content />
+    </div>
+  );
+};
+
+export default App;
