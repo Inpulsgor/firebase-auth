8d905ea06d76c42a91961c56a847ea315577a52f
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2021-01-19 10:25:04 +0200

    tasks section / checkbox styling

--- todo-app/src/components/Tasks/Tasks.jsx
+++ todo-app/src/components/Tasks/Tasks.jsx
@@ -8,7 +8,23 @@ const Tasks = () => {
       <ul className="tasks__list">
         <li className="tasks__item task">
           <input type="checkbox" id="check" />
-          <label htmlFor="check"></label>
+          <label htmlFor="check">
+            <svg
+              width="11"
+              height="8"
+              viewBox="0 0 11 8"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M9.29999 1.20001L3.79999 6.70001L1.29999 4.20001"
+                stroke="white"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </label>
         </li>
       </ul>
     </>
