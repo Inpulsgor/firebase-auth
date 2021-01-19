186ef55d340c1f2ea1701ab389e243d8f060d463
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2021-01-19 14:09:25 +0200

    tasks section / heading edit svg buttov & animation added

--- todo-app/src/components/Tasks/Tasks.jsx
+++ todo-app/src/components/Tasks/Tasks.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 
 import { ReactComponent as CheckedSvg } from "../../assets/icons/checked.svg";
+import { ReactComponent as PenSvg } from "../../assets/icons/pen.svg";
 import "./Tasks.scss";
 
 const Tasks = () => {
   return (
     <>
-      <h2 className="tasks__title">Frontend</h2>
-      <ul className="tasks__list">
-        <li className="tasks__item task">
+      <div className="tasks__heading heading">
+        <h2 className="heading__title">Frontend</h2>
+        <button className="heading__edit">
+          <PenSvg />
+        </button>
+      </div>
+
+      <ul className="tasks__list task">
+        <li className="task__item ">
           <input className="task__input" type="checkbox" id="check" />
           <label htmlFor="check">
             <CheckedSvg />
