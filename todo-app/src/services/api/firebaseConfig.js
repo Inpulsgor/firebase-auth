19b792ea475f2c5fce5f0ca32da48c32d3105e39
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2021-01-25 10:12:53 +0200

    firebase auth in dev

---
+++ todo-app/src/services/api/firebaseConfig.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+  
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+
+  };
+
+  // Initialize Firebase
+  const firebaseInit = firebase.initializeApp(firebaseConfig);
+
+  export default firebaseInit;
