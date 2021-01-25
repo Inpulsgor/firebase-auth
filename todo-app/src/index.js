c25baa606f7a9b243fa754b46161fd3cbeacf2e0
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2021-01-26 00:47:09 +0200

    persistor added

--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -2,15 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from "./components/App/App";
-import { store } from "./redux/store";
+import { store, persistor } from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <Router>
       <App />
     </Router>
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
--- todo-app/src/redux/store.js
+++ todo-app/src/redux/store.js
@@ -1,13 +1,23 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import thunk from "redux-thunk";
 import { logger } from "redux-logger";
 
 import rootReducer from "./rootReducer";
+
+const persistConfig = {
+  key: 'token',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // MIDDLEWARE
 const middleware = [thunk, logger]; // [thunk, ...] <- middleware goes here
 const enhancer = applyMiddleware(...middleware);
 
 // STORE
-export const store = createStore(rootReducer, composeWithDevTools(enhancer));
+export const store = createStore(persistedReducer, composeWithDevTools(enhancer));
+export const persistor = persistStore(store);
