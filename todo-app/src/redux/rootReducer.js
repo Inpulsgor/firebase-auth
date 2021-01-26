95ddc2def7081223f66b0ab439ffa50467c2250b
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2021-01-26 09:18:19 +0200

    persist store for token added

--- todo-app/src/redux/rootReducer.js
+++ todo-app/src/redux/rootReducer.js
@@ -1,13 +1,21 @@
 import { combineReducers } from "redux";
+import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer } from 'redux-persist';
 
 // REDUCERS
 import authReducer from "./auth/authReducer";
 // import listsReducers from "./lists/listsReducers";
 // import ColorsReducers from "./colors/ColorsReducers";
 
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token']
+};
+
 // ROOT REDUCER
 const rootReducer = combineReducers({
-  auth: authReducer,
+  auth: authReducer, persistReducer(authReducer, authPersistConfig)
   // lists: listsReducers,
   // colors: ColorsReducers,
 });
--- todo-app/src/redux/store.js
+++ todo-app/src/redux/store.js
@@ -1,23 +1,22 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 import thunk from "redux-thunk";
 import { logger } from "redux-logger";
 
 import rootReducer from "./rootReducer";
 
-const persistConfig = {
-  key: 'token',
-  storage,
-};
+// const authPersistConfig = {
+//   key: 'auth',
+//   storage,
+// };
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // MIDDLEWARE
 const middleware = [thunk, logger]; // [thunk, ...] <- middleware goes here
 const enhancer = applyMiddleware(...middleware);
 
 // STORE
-export const store = createStore(persistedReducer, composeWithDevTools(enhancer));
+export const store = createStore(rootReducer, composeWithDevTools(enhancer));
 export const persistor = persistStore(store);
